f2849aee96c0524798518fa31c051bbae39c41a6
Shyamkumargundaboina <118732784+Shyamkumargundaboina@users.noreply.github.com>
2022-11-21 18:10:17 +0530

    Update streamlit_app.py
    Need to add more

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
-streamlit.title("My Parents Healthy Dinner")
+streamlit.title('My Parents Healthy Dinner')
+
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text (Omega 3 & blueberry oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streami.text('Hard Boiled Free -Range Eggs')
