09c9b6b214413b4ba28ac5cbe1dc563075f46e0b
Shyamkumargundaboina <118732784+Shyamkumargundaboina@users.noreply.github.com>
2022-11-21 18:56:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text ('Omega 3 & blueberry oatmeal')
 streamlit.text('Kale,Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Free -Range Eggs')
+
+streamlit.header('Build your own fruit smoothie')
